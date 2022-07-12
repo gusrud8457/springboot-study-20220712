47fe695173f7953833221f8d115b65b296d1ae0f
gusrud8457 <fjqm7280@naver.com>
2022-07-12 20:52:17 +0900

    스프링

---
+++ src/test/java/com/springboot/studykyeong/StudyKyeongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.studykyeong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyKyeongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
